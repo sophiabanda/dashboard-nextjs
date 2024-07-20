c8c6b3401c675b26556d6a7696ac6b318747a42d
Sophia M Banda <51414936+sophiabanda@users.noreply.github.com>
2024-07-20 10:50:37 -0400

    Add Suspense to dashbaord cards

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -4,10 +4,12 @@ import RevenueChart from '/Users/sophiabanda/Projects/nextjs-dashboard/app/ui/da
 import LatestInvoices from '/Users/sophiabanda/Projects/nextjs-dashboard/app/ui/dashboard/latest-invoices';
 import { lusitana } from '/Users/sophiabanda/Projects/nextjs-dashboard/app/ui/fonts';
 import { fetchCardData } from '/Users/sophiabanda/Projects/nextjs-dashboard/app/lib/data';
+import CardWrapper from '/Users/sophiabanda/Projects/nextjs-dashboard/app/ui/dashboard/cards';
 
 import {
   RevenueChartSkeleton,
   LatestInvoicesSkeleton,
+  CardsSkeleton,
 } from '/Users/sophiabanda/Projects/nextjs-dashboard/app/ui/skeletons';
 
 export default async function Page() {
@@ -24,14 +26,9 @@ export default async function Page() {
         Dashboard
       </h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-        <Card title="Collected" value={totalPaidInvoices} type="collected" />
-        <Card title="Pending" value={totalPendingInvoices} type="pending" />
-        <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
-        <Card
-          title="Total Customers"
-          value={numberOfCustomers}
-          type="customers"
-        />
+        <Suspense fallback={<CardsSkeleton />}>
+          <CardWrapper />
+        </Suspense>
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
         <Suspense fallback={<RevenueChartSkeleton />}>
