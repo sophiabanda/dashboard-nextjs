ccaa529259288c02110c2ad2b49502ac915dca3a
Sophia M Banda <51414936+sophiabanda@users.noreply.github.com>
2024-07-20 11:00:35 -0400

    Add timeout to test streaming and react suspense

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,9 +1,7 @@
 import React, { Suspense } from 'react';
-import { Card } from '/Users/sophiabanda/Projects/nextjs-dashboard/app/ui/dashboard/cards';
 import RevenueChart from '/Users/sophiabanda/Projects/nextjs-dashboard/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '/Users/sophiabanda/Projects/nextjs-dashboard/app/ui/dashboard/latest-invoices';
 import { lusitana } from '/Users/sophiabanda/Projects/nextjs-dashboard/app/ui/fonts';
-import { fetchCardData } from '/Users/sophiabanda/Projects/nextjs-dashboard/app/lib/data';
 import CardWrapper from '/Users/sophiabanda/Projects/nextjs-dashboard/app/ui/dashboard/cards';
 
 import {
@@ -13,13 +11,6 @@ import {
 } from '/Users/sophiabanda/Projects/nextjs-dashboard/app/ui/skeletons';
 
 export default async function Page() {
-  const {
-    numberOfInvoices,
-    numberOfCustomers,
-    totalPaidInvoices,
-    totalPendingInvoices,
-  } = await fetchCardData();
-
   return (
     <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
--- app/lib/data.ts
+++ app/lib/data.ts
@@ -30,6 +30,9 @@ export async function fetchRevenue() {
 
 export async function fetchLatestInvoices() {
   try {
+    console.log('Fetching revenue data...');
+    await new Promise((resolve) => setTimeout(resolve, 3000));
+
     const data = await sql<LatestInvoiceRaw>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
       FROM invoices
@@ -50,6 +53,7 @@ export async function fetchLatestInvoices() {
 
 export async function fetchCardData() {
   try {
+    await new Promise((resolve) => setTimeout(resolve, 2000));
     // You can probably combine these into a single SQL query
     // However, we are intentionally splitting them to demonstrate
     // how to initialize multiple queries in parallel with JS.
