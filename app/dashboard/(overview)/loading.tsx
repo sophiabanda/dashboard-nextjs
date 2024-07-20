cbe8ce3287c9bc6e5978990ac2d531a87f038251
Sophia M Banda <51414936+sophiabanda@users.noreply.github.com>
2024-07-20 10:38:57 -0400

    Remove unnecessary .tsk extenstions from imports

--- app/dashboard/(overview)/loading.tsx
+++ app/dashboard/(overview)/loading.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import DashboardSkeleton from '/Users/sophiabanda/Projects/nextjs-dashboard/app/ui/skeletons.tsx';
+import DashboardSkeleton from '/Users/sophiabanda/Projects/nextjs-dashboard/app/ui/skeletons';
 
 export default function Loading() {
   return <DashboardSkeleton />;
